1315442d2e1889e42ed75b35dc8435386e5efabb
shriya-31 <kaisthas31@gmail.com>
2021-08-28 17:36:53 +0530

    added the login component commit qa! :qa!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./styles/app.scss";
 import Home from "./components/home";
 import PetList from "./components/petlist";
+import Login from "./componenets/login";
 import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Link to="/petlist">Go to pet List</Link>
+      <Link to="/login">Go to login page </Link>
       <Switch>
-        <Route exact path="/" render={() => <Home />} />
         <Route exact path="/petlist" render={() => <PetList />} />
+        <Route exact path="/login" render={() => <Login />} />
+        <Route exact path="/" render={() => <Home />} />
       </Switch>
     </div>
   );
---
+++ src/components/login.jsx
@@ -0,0 +1,4 @@
+const Login = () => {
+    return <div>login page</div>
+}
+export default Login;
